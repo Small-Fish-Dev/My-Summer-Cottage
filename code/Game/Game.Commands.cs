2a80be30cd041152ae1d6a31fda322b9541327af
ceitine <fluffy00gt@gmail.com>
2023-01-26 01:31:05 +0200

    add spawn model command

---
+++ code/Game/Game.Commands.cs
@@ -0,0 +1,32 @@
+﻿namespace Sauna;
+
+partial class Sauna
+{
+	[ConCmd.Server( "spawn_model" )]
+	public static void SpawnModel( string path )
+	{
+		if ( ConsoleSystem.Caller?.Pawn is not Player pawn ) 
+			return;
+
+		var model = Model.Load( path );
+		if ( model.Name == "models/dev/error.vmdl" )
+		{
+			Log.Error( $"Couldn't load the model: {path}" );
+			return;
+		}
+
+		var ray = new Ray(
+			pawn.Position + Vector3.Up * pawn.CollisionBox.Maxs.z,
+			pawn.ViewAngles.ToRotation().Forward );
+
+		var trace = Trace.Ray( ray, 500f )
+			.Ignore( pawn )
+			.Size( model.Bounds )
+			.Run();
+
+		var entity = new ModelEntity();
+		entity.Model = model;
+		entity.SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+		entity.Position = trace.EndPosition;
+	}
+}
