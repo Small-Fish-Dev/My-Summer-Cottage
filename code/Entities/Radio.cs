1572fb617443081dceeb3db4b1f08ef28cbdfed2
ceitine <fluffy00gt@gmail.com>
2023-01-28 05:33:39 +0200

    radio entity, spawn and delete entity commands

---
+++ code/Entities/Radio.cs
@@ -0,0 +1,101 @@
+﻿namespace Sauna;
+
+public partial class Radio : ModelEntity, IInteractable
+{
+	string IInteractable.DisplayTitle => "Mankka";
+
+	public struct Song
+	{
+		public string Producer;
+		public string Name;
+		public string Path;
+	}
+
+	List<Song> sounds = FileSystem.Mounted.FindFile( "sounds/music/" )
+		.Where( file => file.EndsWith( ".sound" ) )
+		.Select( path =>
+		{
+			var separate = path
+				.Substring( 0, path.Length - 6 )
+				.Replace( '_', ' ' )
+				.Split( "-" );
+			
+			return new Song 
+			{ 
+				Producer = separate[0],
+				Name = separate[1],
+				Path = $"sounds/music/{path}",
+			};
+		} )
+		.ToList();
+
+	Sound? sound;
+	Song? current;
+
+	public bool Playing => sound != null;
+
+	public Radio()
+	{
+		var interactable = this as IInteractable;
+
+		interactable.AddInteraction( InputButton.Use, new()
+		{
+			Predicate = ( Player pawn ) => true,
+			Function = ( Player pawn ) =>
+			{
+				if ( Game.IsClient ) return;
+				
+				if ( Playing )
+				{
+					Stop();
+					return;
+				}
+
+				var random = sounds[Game.Random.Int( sounds.Count - 1 )];
+				Play( random );
+			},
+			Text = "Toggle"
+		} );
+	}
+
+	public override void Spawn()
+	{
+		SetModel( "models/arrow.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+	}
+
+	/// <summary>
+	/// Stops the current song.
+	/// </summary>
+	public void Stop()
+	{
+		Game.AssertServer();
+
+		sound?.Stop();
+		sound = null;
+		current = null;
+	}
+
+	/// <summary>
+	/// Play a song.
+	/// </summary>
+	/// <param name="song"></param>
+	public void Play( Song song )
+	{
+		Game.AssertServer();
+
+		Stop();
+		current = song;
+		sound = Sound.FromEntity( song.Path, this );
+	}
+
+	[Event.Tick.Server]
+	void tick()
+	{
+		if ( sound != null && (sound?.Finished ?? false) )
+		{
+			var random = sounds[Game.Random.Int( sounds.Count - 1 )];
+			Play( random );
+		}
+	}
+}
--- code/Game/Game.Commands.cs
+++ code/Game/Game.Commands.cs
@@ -1,4 +1,6 @@
-﻿namespace Sauna;
+﻿using Sandbox;
+
+namespace Sauna;
 
 partial class Sauna
 {
@@ -27,6 +29,61 @@ partial class Sauna
 		var entity = new ModelEntity();
 		entity.Model = model;
 		entity.SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+		entity.Position = trace.EndPosition;
+	}
+
+	[ConCmd.Server( "delete_entity" )]
+	public static void DeleteEntity()
+	{
+		if ( ConsoleSystem.Caller?.Pawn is not Player pawn )
+			return;
+
+		var ray = new Ray(
+			pawn.Position + Vector3.Up * pawn.CollisionBox.Maxs.z,
+			pawn.ViewAngles.ToRotation().Forward );
+
+		var trace = Trace.Ray( ray, 500f )
+			.Ignore( pawn )
+			.Run();
+
+		if ( trace.Entity == null )
+		{
+			Log.Error( $"Didn't find any entity to delete." );
+			return;
+		}
+
+		trace.Entity?.Delete();
+
+	}
+
+	[ConCmd.Server( "spawn_entity" )]
+	public static void SpawnEntity( string typeName )
+	{
+		if ( ConsoleSystem.Caller?.Pawn is not Player pawn )
+			return;
+
+		var type = GlobalGameNamespace.TypeLibrary.GetType( typeName );
+		if ( !type.IsValid )
+		{
+			Log.Error( $"Couldn't find the type: {typeName}" );
+			return;
+		}
+
+		var ray = new Ray(
+			pawn.Position + Vector3.Up * pawn.CollisionBox.Maxs.z,
+			pawn.ViewAngles.ToRotation().Forward );
+
+		var trace = Trace.Ray( ray, 500f )
+			.Ignore( pawn )
+			.Run();
+
+		var entity = type.Create<Entity>();
+		if ( entity == null )
+		{
+			Log.Error( $"Failed to create the entity: {typeName}" );
+			return;
+		}
+
 		entity.Position = trace.EndPosition;
 	}
 
