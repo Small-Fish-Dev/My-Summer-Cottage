4499f6b7af4bee877ea7bff1d81e308b11ce6adf
ceitine <fluffy00gt@gmail.com>
2023-02-05 22:56:36 +0200

    stove hammer entity

---
+++ code/Entities/Hammer/Stove.cs
@@ -0,0 +1,19 @@
+﻿namespace Sauna;
+
+[HammerEntity]
+[Solid]
+[Model( Model = "models/sauna stove/sauna_stove.vmdl" )]
+public partial class Stove : AnimatedEntity, IInteractable
+{
+	string IInteractable.DisplayTitle => "Kiuas";
+
+	public Stove()
+	{
+
+	}
+
+	public override void Spawn()
+	{
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+	}
+}
