13ccfe310121822ef73f6accba5d6b569a6d2a24
ceitine <fluffy00gt@gmail.com>
2023-02-12 21:27:05 +0200

    wip marker

---
+++ code/Util/Marker.cs
@@ -0,0 +1,14 @@
+﻿namespace Sauna;
+
+public struct Marker
+{
+	private static List<Marker> all = new List<Marker>
+	{
+		new() { Name = "Piss", Position = 0 },
+	};
+	public static IReadOnlyList<Marker> All => all;
+
+	public string Name;
+	public Vector3 Position;
+	public Texture Texture;
+}
