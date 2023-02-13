bb0f6162734abf11a277e0b736fd0fb083f10e26
ceitine <fluffy00gt@gmail.com>
2023-02-13 05:56:33 +0200

    https://i.imgur.com/hRHkByx.png marker drawing

--- code/Util/Marker.cs
+++ code/Util/Marker.cs
@@ -4,11 +4,20 @@ public struct Marker
 {
 	private static List<Marker> all = new List<Marker>
 	{
-		new() { Name = "Piss", Position = 0 },
+		new( "Home", Vector3.Zero, Color.Red ),
 	};
 	public static IReadOnlyList<Marker> All => all;
 
 	public string Name;
 	public Vector3 Position;
+	public Color Color;
 	public Texture Texture;
+
+	public Marker( string name, Vector3 position, Color? color = null, Texture? texture = null )
+	{
+		Name = name;
+		Position = position;
+		Color = color ?? Color.White;
+		Texture = texture;
+	}
 }
