89d96f90cc409e9c394c5600e818b4e68fe0701a
ceitine <fluffy00gt@gmail.com>
2023-02-19 02:17:02 +0200

    door entity base

---
+++ code/Entities/Hammer/Door.cs
@@ -0,0 +1,11 @@
+﻿namespace Sauna;
+
+[HammerEntity]
+[Solid]
+public partial class Door : ModelEntity, IInteractable
+{
+	public override void Spawn()
+	{
+		SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+	}
+}
