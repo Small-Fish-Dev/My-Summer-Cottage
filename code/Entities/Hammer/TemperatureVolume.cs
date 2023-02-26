8f74c8c54a4f89236e08bead636d439b7f8eec3f
ceitine <fluffy00gt@gmail.com>
2023-02-26 20:27:43 +0200

    don't make temperaturevolume collide with solids

--- code/Entities/Hammer/TemperatureVolume.cs
+++ code/Entities/Hammer/TemperatureVolume.cs
@@ -14,6 +14,7 @@ public partial class TemperatureVolume : ModelEntity
 	public override void Spawn()
 	{
 		SetupPhysicsFromModel( PhysicsMotionType.Static );
+		EnableSolidCollisions = false;
 		Tags.Add( "trigger" );
 	}
 
