fda261332fd745ae940614b7d32477bdddecf749
ceitine <fluffy00gt@gmail.com>
2023-03-04 21:27:14 +0200

    add more player camera functionality

--- code/Player/Player.Camera.cs
+++ code/Player/Player.Camera.cs
@@ -1,12 +1,22 @@
 ﻿namespace Sauna;
 
+public struct CameraData
+{
+	public Vector3 Position;
+	public Rotation Rotation;
+	public float FOV;
+	public IEntity Viewer;
+}
+
 partial class Player
 {
 	public Vector3 EyePosition { get; set; }
+	public CameraData CameraData { get; private set; }
 	public AnimatedEntity View { get; private set; }
 	public Ray ViewRay => new Ray( EyePosition, ViewAngles.ToRotation().Forward );
 
 	private float fov;
+	private CameraData? overrideCamera = null;
 
 	public override void ClientSpawn()
 	{
@@ -21,22 +31,42 @@ partial class Player
 		Event.Run( "OnSpawn", this );
 	}
 
+	/// <summary>
+	/// Gets the player's true eye position.
+	/// </summary>
+	/// <returns></returns>
 	public Vector3 GetEyePosition()
 	{
 		var bone = GetAttachment( "eyes" );
 		return (bone?.Position ?? (Position + CollisionBox.Maxs.z)) + Rotation.Forward * 4f;
 	}
 
+	/// <summary>
+	/// Overrides the player's camera.
+	/// </summary>
+	/// <param name="data"></param>
+	public void ApplyCameraOverride( CameraData? data )
+		=> overrideCamera = data;
+
 	protected void CameraSimulate( IClient cl )
 	{
+		// Assign CameraData
 		EyePosition = GetEyePosition();
+		CameraData = overrideCamera == null
+			? new CameraData
+			{
+				Position = EyePosition,
+				Rotation = ViewAngles.ToRotation(),
+				FOV = Screen.CreateVerticalFieldOfView( fov = MathX.LerpTo( fov, Input.Down( InputButton.View ) ? 20f : 70f, 5f * Time.Delta ) ),
+				Viewer = this
+			}
+			: overrideCamera.Value;
 
-		Camera.Position = EyePosition;
-		Camera.Rotation = ViewAngles.ToRotation();
-
-		fov = MathX.LerpTo( fov, Input.Down( InputButton.View ) ? 20f : 70f, 5f * Time.Delta );
-		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( fov );
-		Camera.FirstPersonViewer = this;
+		// Set CameraData values.
+		Camera.Position = CameraData.Position;
+		Camera.Rotation = CameraData.Rotation;
+		Camera.FieldOfView = CameraData.FOV;
+		Camera.FirstPersonViewer = CameraData.Viewer;
 		Camera.ZNear = 3f;
 	}
 
