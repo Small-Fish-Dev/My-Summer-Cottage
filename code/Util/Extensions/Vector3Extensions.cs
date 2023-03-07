5782f9d11c7d7997b0ef347f3efdadf091dc4f23
ceitine <fluffy00gt@gmail.com>
2023-03-08 01:29:05 +0200

    wip speechbubble

---
+++ code/Util/Extensions/Vector3Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sauna;
+
+public static class Vector3Extensions
+{
+	/// <summary>
+	/// Rounds the Vector3.
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static Vector3 Round( this Vector3 value )
+		=> new Vector3( MathF.Floor( value.x + 0.5f ), MathF.Floor( value.y + 0.5f ), MathF.Floor( value.z + 0.5f ) );
+}
