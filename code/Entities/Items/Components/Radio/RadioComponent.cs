0ed0b2f476a397f48508720a6eb7cb12c9e6398d
ceitine <ceitinee@gmail.com>
2023-08-14 13:16:00 +0300

    remove old radio code

--- code/Entities/Items/Components/Radio/RadioComponent.cs
+++ code/Entities/Items/Components/Radio/RadioComponent.cs
@@ -3,6 +3,8 @@
 [Prefab, Title( "Radio Component" )]
 public partial class RadioComponent : ItemComponent
 {
+	public const float INTERACT_COOLDOWN = 0.5f;
+
 	public RadioChannelAsset Channel { get; private set; }
 	public float Time { get; private set; }
 	public TimeSince? ElapsedTime { get; private set; }
@@ -10,27 +12,32 @@ public partial class RadioComponent : ItemComponent
 	private MusicPlayer player;
 	private RadioDisplay display;
 
-	public string Title => player != null
-		? player.Title
-		: Channel?.Name ?? string.Empty;
+	public string Title => (player != null
+		? player.Title == string.Empty ? Channel?.Name : player.Title
+		: Channel?.Name) ?? string.Empty;
 
 	public float? Duration => player != null 
 		? player.Duration 
 		: null;
 
+	[Net] 
+	public TimeSince LastInteracted { get; set; }
+
 	public override void Initialize()
 	{
 		if ( Game.IsServer )
 			Item.Transmit = TransmitType.Always;
-
+		
 		// Toggle the radio.
 		Interactable.AddInteraction( "use", new()
 		{
-			Predicate = ( Player pawn ) => true,
+			Predicate = ( Player pawn ) => LastInteracted >= INTERACT_COOLDOWN,
 			Function = ( Player pawn ) =>
 			{
 				if ( Game.IsClient )
 					return;
+
+				LastInteracted = 0f;
 
 				if ( Channel != null )
 				{
@@ -49,11 +56,13 @@ public partial class RadioComponent : ItemComponent
 		// Switch Channel
 		Interactable.AddInteraction( "reload", new()
 		{
-			Predicate = ( Player pawn ) => Channel != null,
+			Predicate = ( Player pawn ) => Channel != null && LastInteracted >= INTERACT_COOLDOWN,
 			Function = ( Player pawn ) =>
 			{
 				if ( Game.IsClient )
 					return;
+
+				LastInteracted = 0f;
 
 				var index = ((List<RadioChannelAsset>)RadioChannelAsset.All).IndexOf( Channel ) + 1;
 				var next = RadioChannelAsset.All[index % RadioChannelAsset.All.Count];
@@ -64,11 +73,13 @@ public partial class RadioComponent : ItemComponent
 
 		Interactable.AddInteraction( "menu", new()
 		{
-			Predicate = ( Player pawn ) => Channel != null,
+			Predicate = ( Player pawn ) => Channel != null && LastInteracted >= INTERACT_COOLDOWN,
 			Function = ( Player pawn ) =>
 			{
 				if ( Game.IsClient )
 					return;
+
+				LastInteracted = 0f;
 
 				var index = ((List<RadioChannelAsset>)RadioChannelAsset.All).IndexOf( Channel ) - 1;
 				var previous = RadioChannelAsset.All[index < 0 ? RadioChannelAsset.All.Count - 1 : index];
@@ -100,24 +111,22 @@ public partial class RadioComponent : ItemComponent
 	/// </summary>
 	/// <param name="target"></param>
 	/// <param name="channel"></param>
-	/// <param name="skipTime"></param>
 	public void Play( To? target = null, RadioChannelAsset channel = null )
 	{
 		Game.AssertServer();
 
 		Channel = channel;
-		/*if ( Channel != null )
-			Time = Sandbox.Time.Now - skipTime;*/
-		
-		// Load file on client and start playing at desired time.
+
+		// Tell our client to play the music!
 		playOnClient( target ?? To.Everyone, Channel?.Name ?? string.Empty );
 	}
 
 	[ClientRpc]
-	private async void playOnClient( string name)
+	private void playOnClient( string name )
 	{
 		player?.Stop();
 		player?.Dispose();
+		player = null;
 
 		ElapsedTime = null;
 
@@ -131,59 +140,21 @@ public partial class RadioComponent : ItemComponent
 
 		player = MusicPlayer.PlayUrl( Channel.URL );
 		player.Entity = Entity;
-		player.Volume = 0.2f;
-		// player.Seek( startTime );
+		player.Volume = 0.8f;
+		player.DistanceMax = 500f;
+		player.DistanceMin = 200f;
+		player.Repeat = true;
 
 		ElapsedTime = 0f;
 	}
 
 	public override void OnDestroy()
 	{
-		if ( !Game.IsClient )
-			return;
-
-		player?.Stop();
 		player?.Dispose();
+		player = null;
+
 		display?.Delete( true );
 	}
-
-	/*[GameEvent.Tick]
-	void tick()
-	{
-		// Pick a new random song.
-		if ( Game.IsServer && ElapsedTime > CurrentSong?.Duration + 1f )
-		{
-			var random = sounds[Game.Random.Int( sounds.Count - 1 )];
-			Play( song: random );
-
-			return;
-		}
-
-		if ( Game.IsClient && display == null )
-			display = new( this );
-
-		if ( stream == null || !stream.IsValid() || decoder == null )
-			return;
-
-		// Handle reading the samples and writing them to the SoundStream.
-		var delay = (float)stream.MaxWriteSampleCount / decoder.SampleRate / 2f; // Give room for sample writes.
-		if ( lastWritten < delay || stream.QueuedSampleCount > 0 )
-			return;
-
-		var buffer = new short[QOA.Base.MaxFrameSamples];
-		var read = decoder.ReadSamples( buffer );
-
-		if ( read == -1 )
-		{
-			stream.Delete();
-			stream = null;
-
-			return;
-		}
-
-		stream.WriteData( buffer.AsSpan() );
-		lastWritten = 0f;
-	}*/
 
 	[SaunaEvent.OnSpawn]
 	static void onSpawn( Player player )
