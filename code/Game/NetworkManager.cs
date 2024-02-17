19538048f1df934aba50933a4d3556a7c113fdc3
ceitine <ceitinee@gmail.com>
2024-02-17 16:11:46 +0200

    initial net manager

---
+++ code/Game/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿namespace Sauna;
+
+public sealed class NetworkManager : Component, Component.INetworkListener
+{
+	[Property] public GameObject Prefab { get; set; }
+
+	/*void INetworkListener.OnConnected( Connection connection )
+	{
+	}*/
+
+	protected override void OnStart()
+	{
+		if ( !GameNetworkSystem.IsActive )
+			GameNetworkSystem.CreateLobby();
+	}
+
+	void INetworkListener.OnActive( Connection connection )
+	{
+		var obj = Prefab.Clone();
+
+		var player = obj.Components.Get<Player>( FindMode.EverythingInSelfAndDescendants );
+		player.Setup( connection );
+		obj.NetworkSpawn( connection );
+	}
+
+	void INetworkListener.OnDisconnected( Connection connection )
+	{
+	}
+
+	/*void INetworkListener.OnBecameHost( Connection previousHost )
+	{
+	}*/
+}
--- code/Globals.cs
+++ code/Globals.cs
@@ -7,3 +7,4 @@ global using System.IO;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using System.Collections.Generic;
+global using Sandbox.Network;
