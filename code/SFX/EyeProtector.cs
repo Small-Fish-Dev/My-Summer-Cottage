de2d644048809ef15736c9c4d8103410fce40ac3
ceitine <ceitinee@gmail.com>
2024-02-17 16:33:24 +0200

    bring back dithering, todo eyeprotector

---
+++ code/SFX/EyeProtector.cs
@@ -0,0 +1,30 @@
+﻿namespace Sauna.SFX;
+
+// TODO: fix
+public class EyeProtector : Component, Component.ExecuteInEditor
+{
+	/*private IDisposable hook;
+	private RenderAttributes attributes = new RenderAttributes();
+
+	public void Render( SceneCamera camera )
+	{
+		var shader = Material.FromShader( "shaders/saunaDither.shader" );
+
+		Graphics.GrabFrameTexture( renderAttributes: attributes );
+		Graphics.Blit( shader, attributes );
+	}
+
+	protected override void OnEnabled()
+	{
+		hook?.Dispose();
+
+		var camera = Components.Get<CameraComponent>( FindMode.InSelf );
+		hook = camera.AddHookAfterUI( "Dither", 99, Render );
+	}
+
+	protected override void OnDisabled()
+	{
+		hook?.Dispose();
+		hook = null;
+	}*/
+}
