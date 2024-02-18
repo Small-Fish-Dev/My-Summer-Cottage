2bd54987f0171e20990de5669351e1ec60ebe929
Matthew Zegar <matthewzegar@gmail.com>
2024-02-18 12:54:05 -0500

    Provide option to disable interaction

--- code/Interaction/Interaction.cs
+++ code/Interaction/Interaction.cs
@@ -2,18 +2,30 @@ namespace Sauna;
 
 public class Interaction : Component
 {
-	public delegate void InteractionEvent( Player interactor, GameObject obj );
-
+	/// <summary>
+	/// The keybind used to trigger this interaction
+	/// </summary>
 	[Property]
 	[InputAction]
 	public string Keybind { get; set; }
 
+	/// <summary>
+	/// The UI description displayed when interacting
+	/// </summary>
 	[Property]
 	public string Description { get; set; }
 
-	[Property]
-	public Color Color { get; set; } = Color.White;
-
+	/// <summary>
+	/// The action that is performed when interacted with
+	/// </summary>
 	[Property]
 	public InteractionEvent Action { get; set; }
+	public delegate void InteractionEvent( Player interactor, GameObject obj );
+
+	/// <summary>
+	/// Whether or not the interaction is able to be performed
+	/// </summary>
+	[Property]
+	public Func<bool> Disabled { get; set; }
+	public bool IsInteractable => Disabled is null || !Disabled();
 }
