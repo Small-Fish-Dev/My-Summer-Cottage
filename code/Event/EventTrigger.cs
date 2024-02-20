337aa0ea7a37e77331c120917fea78666a026daa
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-20 23:10:03 +0100

    EventSystem, Method Polling

--- code/Event/EventTrigger.cs
+++ code/Event/EventTrigger.cs
@@ -1,9 +1,22 @@
 using Sandbox;
+using static Sauna.Event.EventTrigger;
+
+namespace Sauna.Event;
 
 [Hide]
 public abstract class EventTrigger : Component
 {
 	public event Action<GameObject> OnTrigger;
+
+	public virtual bool IsPolled { get; set; } = false;
+
+	/// <summary>
+	/// Maximum ticks it can go without running the polled method
+	/// </summary>
+	[Property, HideIf( "IsPolled", false )]
+	public virtual int MaxPollingRate { get; set; } = 5;
+
+	public int LastPoll = 0;
 
 	protected override void OnStart()
 	{
@@ -11,11 +24,18 @@ public abstract class EventTrigger : Component
 
 	protected override void OnUpdate()
 	{
+	}
+
+	/// <summary>
+	/// Run polled code here
+	/// </summary>
+	public virtual void PolledMethod()
+	{
 
 	}
 
 	// Can't invoke events outside of the class itself even if it derives from this, so we have this method instead
-	protected void CallTrigger( GameObject triggerer )
+	protected virtual void CallTrigger( GameObject triggerer )
 	{
 		OnTrigger?.Invoke( triggerer );
 	}
