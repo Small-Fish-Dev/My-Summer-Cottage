c6fcdda47e3ec4e5d6da8ea187b4862b7b37ab76
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-22 00:46:18 +0100

    EventDefinition

--- code/Event/EventComponent.cs
+++ code/Event/EventComponent.cs
@@ -1,10 +1,9 @@
 using Sandbox;
 using Sauna.Event;
 
-[Icon( "event" )]
-[EditorHandle( "/textures/gizmo/event.png" )]
+[Icon( "flash_on" )]
 [Category( "Events" )]
-public sealed class EventComponent : Component, Component.ExecuteInEditor
+public sealed class EventComponent : Component
 {
 	[Property]
 	public List<EventTrigger> Triggers { get; set; }
@@ -12,42 +11,11 @@ public sealed class EventComponent : Component, Component.ExecuteInEditor
 	[Property]
 	public Action<GameObject> Event { get; set; }
 
-	bool _showToggle = false;
-
-	protected override void DrawGizmos()
-	{
-		// It's done inside of here because this is when we can detect if it's been selected
-		if ( Game.IsEditor )
-		{
-			var shouldShow = GameManager.ActiveScene == GameObject || Gizmo.IsSelected;
-			// The components inside are enabled if you're inside of the prefab or you have the prefab selected
-
-			if ( _showToggle != shouldShow )
-			{
-				foreach ( var component in Components.GetAll( FindMode.EverythingInSelfAndChildren ) )
-				{
-					if ( component != this )
-						component.Enabled = shouldShow;
-				}
-
-				_showToggle = shouldShow;
-			}
-
-		}
-	}
 
 	protected override void OnStart()
 	{
-		foreach ( var component in Components.GetAll( FindMode.EverythingInSelfAndChildren ) )
-		{
-			if ( component != this )
-				component.Enabled = true; // Make sure to enable back all the components in case they were disabled
-		}
-
 		foreach ( var trigger in Triggers )
-		{
 			trigger.OnTrigger += Event;
-		}
 	}
 
 	protected override void OnUpdate()
---
+++ code/Event/EventDefinition.cs
@@ -0,0 +1,108 @@
+using Sandbox;
+
+public enum EventFrequency
+{
+	[Icon( "menu_book" )]
+	[Description( "Events that are run at specific times in the story. Doesn't get added to the daily events pool" )]
+	Story,
+	[Icon( "stars" )]
+	[Description( "Events that are run once and will never happen again. Can get added to the daily events pool" )]
+	Unique,
+	[Icon( "timeline" )]
+	[Description( "Events that can run multiple times. Can get added to the daily events pool" )]
+	Repeatable
+}
+
+public enum EventType
+{
+	[Icon( "not_listed_location" )]
+	[Description( "This event will trigger randomly during the day" )]
+	Random,
+	[Icon( "view_in_ar" )]
+	[Description( "This event requires you to show up somewhere to trigger" )]
+	Zone,
+	[Icon( "touch_app" )]
+	[Description( "This event requires you interacting with something to trigger" )]
+	Interaction,
+	[Icon( "event_busy" )]
+	[Description( "This event has an external factor to trigger. (Like Story events)" )]
+	Direct
+}
+
+public enum EventRarity
+{
+	[Icon( "grid_on" )]
+	Common,
+	[Icon( "window" )]
+	Uncommon,
+	[Icon( "check_box_outline_blank" )]
+	Rare,
+	[Icon( "grid_off" )]
+	None,
+}
+
+[Icon( "event" )]
+[EditorHandle( "/textures/gizmo/event.png" )]
+[Category( "Events" )]
+public sealed class EventDefinition : Component, Component.ExecuteInEditor
+{
+	[Property]
+	public string EventName { get; set; }
+
+	[Property]
+	public EventType Type { get; set; } = EventType.Random;
+
+	[Property]
+	public EventFrequency Frequency { get; set; } = EventFrequency.Repeatable;
+
+	[Property]
+	[HideIf( "Frequency", EventFrequency.Story )]
+	public EventRarity Rarity { get; set; } = EventRarity.Common;
+
+	[Property]
+	[Description( "Can this event trigger when there's another event going on? And can other events trigger whn this one is going on?" )]
+	public bool Stackable { get; set; } = true;
+
+
+	public bool HasBeenPlayed { get; set; } = false;
+
+
+	bool _showToggle = false;
+
+	protected override void DrawGizmos()
+	{
+		// It's done inside of here because this is when we can detect if it's been selected
+		if ( Game.IsEditor )
+		{
+			var shouldShow = GameManager.ActiveScene == GameObject || Gizmo.IsSelected;
+			// The components inside are enabled if you're inside of the prefab or you have the prefab selected
+
+			if ( _showToggle != shouldShow )
+			{
+				foreach ( var component in Components.GetAll( FindMode.EverythingInSelfAndChildren ) )
+				{
+					if ( component != this )
+						component.Enabled = shouldShow;
+				}
+
+				_showToggle = shouldShow;
+			}
+
+		}
+	}
+
+	protected override void OnStart()
+	{
+		if ( Game.IsEditor ) return;
+
+		foreach ( var component in Components.GetAll( FindMode.EverythingInSelfAndChildren ) )
+		{
+			if ( component != this )
+				component.Enabled = true; // Make sure to enable back all the components in case they were disabled
+		}
+	}
+
+	protected override void OnUpdate()
+	{
+	}
+}
