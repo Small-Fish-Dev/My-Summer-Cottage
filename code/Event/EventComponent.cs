032d799f6bbd0df9f0a331de7b504ffa60b802c9
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-22 01:07:17 +0100

    Event Components TriggerOnce and Triggered

--- code/Event/EventComponent.cs
+++ code/Event/EventComponent.cs
@@ -11,15 +11,68 @@ public sealed class EventComponent : Component
 	[Property]
 	public Action<GameObject> Event { get; set; }
 
+	[Property]
+	[Description( "This event component will only trigger once" )]
+	public bool TriggerOnce { get; set; } = true;
+
+	bool _triggered = false;
+
+	/// <summary>
+	/// Has this event component been triggered?
+	/// </summary>
+	public bool Triggered
+	{
+		get => _triggered;
+		set
+		{
+			if ( TriggerOnce )
+			{
+				if ( value == true && Triggered == false )
+				{
+					foreach ( var trigger in Triggers )
+					{
+						trigger.OnTrigger -= Event;
+						trigger.OnTrigger -= HasBeenTriggered;
+					}
+				}
+
+				if ( value == false && Triggered == true )
+				{
+					foreach ( var trigger in Triggers )
+					{
+						trigger.OnTrigger += Event;
+						trigger.OnTrigger += HasBeenTriggered;
+					}
+				}
+			}
+
+			_triggered = value;
+		}
+	}
+
 
 	protected override void OnStart()
 	{
 		foreach ( var trigger in Triggers )
+		{
 			trigger.OnTrigger += Event;
+			trigger.OnTrigger += HasBeenTriggered;
+		}
+	}
+
+	void HasBeenTriggered( GameObject _ )
+	{
+		if ( TriggerOnce )
+			Triggered = true;
+	}
+
+	protected override void OnEnabled()
+	{
+		if ( Triggered )
+			Triggered = false; // Reset if it's reenabled
 	}
 
 	protected override void OnUpdate()
 	{
-
 	}
 }
