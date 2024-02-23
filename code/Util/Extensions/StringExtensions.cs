250aa32354f68cfbb24199fec74e3e180b0fa12e
ceitine <ceitinee@gmail.com>
2024-02-24 00:34:21 +0200

    remove diacritics on radio display

---
+++ code/Util/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Sauna;
+
+public static class StringExtensions
+{
+	public static string RemoveDiacritics( this string text )
+	{
+		var normalizedString = text.Normalize( NormalizationForm.FormD );
+		var stringBuilder = new StringBuilder( capacity: normalizedString.Length );
+
+		for ( int i = 0; i < normalizedString.Length; i++ )
+		{
+			char c = normalizedString[i];
+			var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory( c );
+			if ( unicodeCategory != UnicodeCategory.NonSpacingMark )
+			{
+				stringBuilder.Append( c );
+			}
+		}
+
+		return stringBuilder
+			.ToString()
+			.Normalize( NormalizationForm.FormC );
+	}
+}
