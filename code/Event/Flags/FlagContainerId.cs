9e506a518097645fe47f324c0ea4b1c153d48c93
Gio <lotuspar0@gmail.com>
2024-02-24 16:09:05 +0800

    Delete flags

--- code/Event/Flags/FlagContainerId.cs
+++
@@ -1,8 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace Sauna.Event.Flags;
-
-public enum FlagContainerId
-{
-	INVALID = 0,
-	PLAYER_CONTAINER,
-}
--- code/Event/Flags/FlagId.cs
+++
@@ -1,6 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace Sauna.Event.Flags;
-
-public enum FlagId
-{
-}
