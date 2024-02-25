47c110e207e9c0ca66e47000fabd85b482d4981e
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-25 14:59:26 +0100

    IsSingleplayer node

---
+++ code/Event/Nodes/GlobalNodes.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using Sandbox.Utility;
+using Sauna;
+using Sauna.Game;
+
+public static partial class GlobalNodes
+{
+	/// <summary>
+	/// Is the game being played by a single player
+	/// </summary>
+	[ActionGraphNode( "event.issingleplayer" ), Pure]
+	[Title( "Is Single Player" ), Group( "Events" ), Icon( "emoji_people" )]
+	public static bool IsSingleplayer()
+	{
+		return Networking.Connections.Count > 1; // lol! :-)
+	}
+}
