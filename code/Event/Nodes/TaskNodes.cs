df239d780ce70ccb44c263b5107cd25f5cf4dc4c
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-28 17:47:33 +0100

    AssignTaskTo node, AssignTaskToEveryone node

---
+++ code/Event/Nodes/TaskNodes.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using Sandbox.Utility;
+using Sauna;
+using Sauna.Event;
+
+public static partial class TaskNodes
+{
+	/// <summary>
+	/// Add a task to the player's current tasks
+	/// </summary>
+	/// <param name="taskToAdd"></param>
+	/// <param name="player"></param>
+	[ActionGraphNode( "event.assigntaskto" )]
+	[Title( "Assign New Task To" ), Group( "Events" ), Icon( "edit_calendar" )]
+	public static void AssignTaskTo( SaunaTask taskToAdd, Player player )
+	{
+		if ( player != null )
+			TaskMaster.AssignNewTask( taskToAdd, player );
+	}
+
+	/// <summary>
+	/// Add a task to all of the players current tasks
+	/// </summary>
+	/// <param name="taskToAdd"></param>
+	[ActionGraphNode( "event.assigntasktoeveryone" )]
+	[Title( "Assign Everyone New Task" ), Group( "Events" ), Icon( "edit_calendar" )]
+	public static void AssignTaskToEveryone( SaunaTask taskToAdd )
+	{
+		TaskMaster.AssignEveryoneNewTask( taskToAdd );
+	}
+}
