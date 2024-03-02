3d89b7267f93462ee203ba5912bc303c6d6b1c58
ceitine <ceitinee@gmail.com>
2024-03-02 20:12:52 +0200

    tinter component

---
+++ code/Components/TinterComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace Sauna;
+
+public sealed class TinterComponent : Component, Component.ExecuteInEditor
+{
+	[Property] public ModelRenderer Renderer { get; set; }
+	[Property] public Color Tint
+	{
+		get => _tint;
+		set
+		{
+			_tint = value;
+
+			var obj = Renderer?.SceneObject;
+			if ( obj != null )
+				obj.Attributes.Set( "g_flColorTint", _tint );
+		}
+	}
+	
+	private Color _tint;
+}
