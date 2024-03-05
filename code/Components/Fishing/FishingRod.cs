7f5b0618274649aec0874b5ddb5a5d38ce9f4dbb
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2024-03-05 06:39:14 +0300

    Add a bobber to the fishing rod

--- code/Components/Fishing/FishingRod.cs
+++ code/Components/Fishing/FishingRod.cs
@@ -2,7 +2,13 @@
 
 public sealed class FishingRod : Component
 {
-	[Sync] public bool Casted { get; set; }
+	[Property] public GameObject BobberPrefab;
+	[Property] public float RetractDistance = 200;
+	[Property] public float ThrowForce = 100;
+
+	private GameObject CurrentBobber { get; set; }
+	private GameObject Owner { get; set; }
+	public bool Casted => CurrentBobber.IsValid();
 
 	protected override void OnStart()
 	{
@@ -11,12 +17,38 @@ public sealed class FishingRod : Component
 		interactions.AddInteraction( new Interaction
 		{
 			Accessibility = AccessibleFrom.Hands,
-			Action = ( Player player, GameObject obj ) =>
-			{
-				Casted = !Casted;
-			},
+			Action = OnInteract,
 			DynamicText = () => Casted ? "Pull back" : "Cast",
 			Keybind = "mouse1"
 		} );
+	}
+
+	protected override void OnUpdate()
+	{
+		if (Casted && CurrentBobber.Transform.Position.Distance( Owner.Transform.Position ) > RetractDistance)
+			RetractBobber();
+	}
+
+	private void OnInteract( Player player, GameObject obj )
+	{
+		if ( Casted )
+		{
+			RetractBobber();
+		}
+		else
+		{
+			Owner = player.GameObject;
+			CurrentBobber = BobberPrefab.Clone();
+			CurrentBobber.Enabled = true;
+			CurrentBobber.Transform.Position = player.Transform.Position + player.Bounds.Center;
+			CurrentBobber.Components.Get<Rigidbody>().Velocity = player.Velocity + player.EyeAngles.Forward * ThrowForce;
+		}
+	}
+
+	private void RetractBobber()
+	{
+		CurrentBobber.Destroy();
+		CurrentBobber = null;
+		Owner = null;
 	}
 }
