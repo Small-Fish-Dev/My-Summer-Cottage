4476af57bab442ea0f99fdeb9e7dc324ae19368e
ceitine <ceitinee@gmail.com>
2024-03-05 21:02:23 +0200

    fish weight range and assignweight method

--- code/Fishing/Fish.cs
+++ code/Fishing/Fish.cs
@@ -4,7 +4,26 @@ public class Fish : Component
 {
 	[Property] [Range( 0, 1 )] public float Rarity { get; set; }
 
-	[Property] public float MinimumWaterDepth = 10;
-	[Property] public RangedFloat Weight = new RangedFloat( 1, 10 );
-	[Property] public int CostPerKilo = 1;
+	[Property] public float MinimumWaterDepth { get; set; } = 10;
+
+	/// <summary>
+	/// The fish species' weight range in grams.
+	/// </summary>
+	[Property] public RangedFloat WeightRange { get; set; } = new RangedFloat( 100, 10000 );
+	[Property] public int CostPerKilo { get; set; } = 1;
+
+	/// <summary>
+	/// Assigns a weight to the fishs' ItemComponent in grams.
+	/// Also updates the sell price based on CostPerKilo.
+	/// </summary>
+	/// <param name="weight"></param>
+	public void AssignWeight( int weight )
+	{
+		var item = Components.Get<ItemComponent>( FindMode.EverythingInSelfAndParent );
+		if ( item == null )
+			return;
+
+		item.WeightInGrams = weight;
+		item.SellPrice = (int)(weight / 1000f * CostPerKilo);
+	}
 }
