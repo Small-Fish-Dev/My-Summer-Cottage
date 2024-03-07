79822f92e83b614f95db069d950619957cb071b4
ceitine <ceitinee@gmail.com>
2024-03-07 06:05:57 +0200

    trash "fish" prefabs

--- code/Fishing/Fish.cs
+++ code/Fishing/Fish.cs
@@ -3,14 +3,15 @@
 public class Fish : Component
 {
 	[Property] [Range( 0, 1 )] public float Rarity { get; set; }
+	[Property] public bool IsTrash { get; set; }
 
 	[Property] public float MinimumWaterDepth { get; set; } = 10;
 
 	/// <summary>
 	/// The fish species' weight range in grams.
 	/// </summary>
-	[Property] public RangedFloat WeightRange { get; set; } = new RangedFloat( 100, 10000 );
-	[Property] public int CostPerKilo { get; set; } = 1;
+	[Property, HideIf( "IsTrash", true )] public RangedFloat WeightRange { get; set; } = new RangedFloat( 100, 10000 );
+	[Property, HideIf( "IsTrash", true )] public int CostPerKilo { get; set; } = 1;
 
 	/// <summary>
 	/// Assigns a weight to the fishs' ItemComponent in grams.
@@ -19,11 +20,14 @@ public class Fish : Component
 	/// <param name="weight"></param>
 	public void AssignWeight( int weight )
 	{
+		if ( IsTrash )
+			return;
+
 		var item = Components.Get<ItemComponent>( FindMode.EverythingInSelfAndParent );
 		if ( item == null )
 			return;
 
 		item.WeightInGrams = weight;
-		item.SellPrice = (int)(weight / 1000f * CostPerKilo);
+		item.SellPrice = (int)(weight / 1000f * CostPerKilo + 0.5f);
 	}
 }
