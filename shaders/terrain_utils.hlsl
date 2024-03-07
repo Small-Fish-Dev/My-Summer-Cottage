e9787ad9638bcb8bbed649ae41b252f44aa2925c
wheatleymf <wheatleyinthespace@yandex.ru>
2024-03-07 14:56:32 +0300

    Add BNW function var in terrain_utils.hlsl

--- shaders/terrain_utils.hlsl
+++ shaders/terrain_utils.hlsl
@@ -1,15 +1,28 @@
 #if ( PROGRAM == VFX_PROGRAM_PS )
 float3 ApplyWithSplatdata( float4 l1, float4 l2, float4 l3, float4 l4, float4 splatmap )
 {
-    float l2blend = ComputeBlendWeight( splatmap.g, 0.1, l2.a );
-    float l3blend = ComputeBlendWeight( splatmap.b, 0.1, l3.a );
-    float l4blend = ComputeBlendWeight( splatmap.a, 0.1, l4.a );
+    float l2blend = ComputeBlendWeight( splatmap.g, 0.05, l2.a );
+    float l3blend = ComputeBlendWeight( splatmap.b, 0.05, l3.a );
+    float l4blend = ComputeBlendWeight( splatmap.a, 0.05, l4.a );
 
     l2.rgb = lerp( l1.rgb, l2.rgb, l2blend );
     l3.rgb = lerp( l2.rgb, l3.rgb, l3blend );
     l4.rgb = lerp( l3.rgb, l4.rgb, l4blend );
 
     return l4.rgb;
+}
+
+float ApplyWithSplatdataBNW( float2 l1, float2 l2, float2 l3, float2 l4, float4 splatmap )
+{
+    float l2blend = ComputeBlendWeight( splatmap.g, 0.05, l2.g );
+    float l3blend = ComputeBlendWeight( splatmap.b, 0.05, l3.g );
+    float l4blend = ComputeBlendWeight( splatmap.a, 0.05, l4.g );
+
+    l2.r = lerp( l1.r, l2.r, l2blend );
+    l3.r = lerp( l2.r, l3.r, l3blend );
+    l4.r = lerp( l3.r, l4.r, l4blend );
+
+    return l4.r;
 }
 
 // Apply textures onto geometry with assigned UV. This is probably a horrible hack and there's a better way to do this. 
