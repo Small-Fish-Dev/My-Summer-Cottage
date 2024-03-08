36e4fde538a49025aeb1c6ce9008f8697b4b1e82
yuberee <59583743+yuberee@users.noreply.github.com>
2024-03-08 01:06:47 +0100

    NPCType and Ragdolling and Footsteps

--- code/Components/NPC/NPC.cs
+++ code/Components/NPC/NPC.cs
@@ -1,11 +1,32 @@
-using Sandbox;
+﻿using Sandbox;
 using Sauna;
+
+public enum NPCType
+{
+	[Icon( "🐇" )]
+	[Description( "Killed by low damage" )]
+	Small,
+	[Icon( "🧍" )]
+	[Description( "Stunned by low damage, killed by medium damage" )]
+	Medium,
+	[Icon( "🦌" )]
+	[Description( "Stunned by medium damage, killed by large damage" )]
+	Large,
+	[Icon( "👼" )]
+	[Description( "Can't be killed" )]
+	Immortal
+}
 
 [Icon( "smart_toy" )]
 public class NPC : Component
 {
 	[Property]
 	public SkinnedModelRenderer Model { get; set; }
+
+	[Property]
+	[Title( "NPC Type" )]
+	public NPCType NPCType { get; set; } = NPCType.Medium;
+
 
 	[Property]
 	[Category( "Stats" )]
@@ -51,6 +72,9 @@ public class NPC : Component
 
 		Tags.Set( "npc", true );
 		CurrentSpeed = WalkSpeed;
+
+		if ( Model != null )
+			Model.OnFootstepEvent += OnFootstep;
 	}
 
 	void SetupNavAgent()
@@ -106,12 +130,18 @@ public class NPC : Component
 				var newRotation = Rotation.LookAt( TargetObject.Transform.Position.WithZ( 0f ) - Transform.Position.WithZ( 0f ) );
 				Transform.Rotation = Rotation.Lerp( Transform.Rotation, newRotation, Time.Delta * 5f );
 				Agent.UpdateRotation = false;
+
+				SetRagdoll( true );
 			}
 			else
 				Agent.UpdateRotation = FaceTowardsVelocity;
 		}
 		else
 			Agent.UpdateRotation = FaceTowardsVelocity;
+
+		if ( Ragdoll != null )
+			FollowRagdoll();
+
 	}
 
 	void CheckNewTargetPos()
@@ -252,5 +282,198 @@ public class NPC : Component
 			return closestObjectPoint.Value;
 		else
 			return position;
+	}
+
+	// RAGDOLL vvv
+
+	public ModelPhysics Ragdoll => Model.Components.Get<ModelPhysics>();
+	SkinnedModelRenderer _puppet;
+	bool _isTransitioning = false;
+	[Sync] RealTimeUntil _unragdoll { get; set; }
+	Vector3 _lastPosition = Vector3.Zero;
+	float _spin = 0f;
+
+	/// <summary>
+	/// Set the ragdoll state of the NPC
+	/// </summary>
+	/// <param name="ragdoll">Ragdoll or Unragdoll</param>
+	/// <param name="duration">How long ragdoll state lasts</param>
+	/// <param name="spin">How fast it spins towards the given velocity</param>
+	[Broadcast( NetPermission.Anyone )]
+	public void SetRagdoll( bool ragdoll, float duration = 2f, float spin = 10f )
+	{
+		if ( ragdoll )
+		{
+			if ( Ragdoll == null && Model != null )
+			{
+				var newRagdoll = Model.Components.Create<ModelPhysics>();
+
+				newRagdoll.Model = Model.Model;
+				newRagdoll.Renderer = Model;
+
+				newRagdoll.Enabled = false;
+				newRagdoll.Enabled = true; // Gotta call OnEnabled for it to update :)
+
+				if ( Agent != null )
+					newRagdoll.PhysicsGroup.Velocity = Agent.Velocity;
+
+				_puppet = Model.GameObject.Parent.Components.Create<SkinnedModelRenderer>();
+				_puppet.Model = Model.Model;
+				_puppet.Enabled = false;
+				_puppet.Enabled = true;
+				_puppet.SceneModel.RenderingEnabled = false;
+
+				foreach ( var collider in Components.GetAll<Collider>( FindMode.EverythingInSelfAndChildren ) )
+					collider.Enabled = false;
+			}
+
+			_unragdoll = duration;
+			_lastPosition = Ragdoll.Transform.World.Position;
+			_spin = spin;
+		}
+		else
+		{
+			_unragdoll = 0f;
+		}
+	}
+
+	void FollowRagdoll()
+	{
+		if ( Ragdoll == null ) return;
+
+		var rootPosition = Transform.Position;
+
+		if ( Model.GetAttachment( "foot_L" ) != null )
+		{
+			var leftFoot = Model.GetAttachment( "foot_L" ).Value.Position;
+			var rightFoot = Model.GetAttachment( "foot_R" ).Value.Position;
+			rootPosition = (leftFoot + rightFoot) / 2f;
+		}
+
+		if ( !_isTransitioning )
+		{
+			Transform.Position = rootPosition; // Remember to set before Renderer!
+			Model.Transform.Local = new Transform( Vector3.Zero, Rotation.Identity ); // Model goes offset
+		}
+
+		var velocity = (Ragdoll.Transform.World.Position - _lastPosition);
+		_lastPosition = Ragdoll.Transform.World.Position;
+
+		var horizontalDirection = velocity.WithZ( 0f ).Normal;
+		var rotatedDirection = horizontalDirection.RotateAround( 0f, Rotation.FromYaw( 90f ) );
+
+		Ragdoll.PhysicsGroup.AngularVelocity = rotatedDirection * _spin;
+
+		if ( _unragdoll )
+		{
+			if ( !_isTransitioning )
+			{
+				_unragdoll = 0f;
+
+				var groundTrace = Scene.Trace.Ray( rootPosition, rootPosition + Vector3.Down * 10f )
+					.Size( 20f )
+					.IgnoreGameObjectHierarchy( GameObject )
+					.WithoutTags( "player", "trigger", "npc" )
+					.Run();
+
+				if ( groundTrace.Hit )
+					_isTransitioning = true;
+			}
+			else
+			{
+				var transition = 0.15f;
+
+				var bones = _puppet.Model.Bones.AllBones;
+				Dictionary<PhysicsBody, Transform> bodyTransforms = new();
+
+				foreach ( var bone in bones )
+				{
+					var body = Ragdoll.PhysicsGroup.GetBody( bone.Index );
+
+					if ( body != null )
+						bodyTransforms.Add( body, body.Transform );
+				}
+
+				if ( _unragdoll.Passed <= transition )
+				{
+					if ( Components.TryGet<Character>( out var character, FindMode.EnabledInSelfAndChildren ) )
+					{
+						_puppet.SceneModel.Morphs.Set( "fat", character.Fatness );
+						_puppet.Set( "height", character.Height );
+					}
+
+					var time = _unragdoll.Passed / transition;
+
+					foreach ( var bone in bones )
+					{
+						if ( _puppet.TryGetBoneTransform( in bone, out var transform ) )
+						{
+							var body = Ragdoll.PhysicsGroup.GetBody( bone.Index );
+
+							if ( body != null )
+							{
+								body.MotionEnabled = false;
+								body.GravityEnabled = false;
+								body.EnableSolidCollisions = false;
+								body.LinearDrag = 9999f;
+								body.AngularDrag = 9999f;
+
+								var oldTransform = bodyTransforms[body];
+								var newPos = oldTransform.Position.LerpTo( transform.Position, (float)time );
+								var newRot = Rotation.Lerp( oldTransform.Rotation, transform.Rotation, (float)time );
+
+								body.Position = newPos;
+								body.Rotation = newRot;
+							}
+						}
+					}
+				}
+				else
+				{
+					_puppet.Destroy();
+					Ragdoll?.Destroy();
+
+					Model.Transform.Local = new Transform( Vector3.Zero, Rotation.Identity ); // Model goes offset
+
+					foreach ( var clothing in Model.GameObject.Children )
+						clothing.Transform.Local = new Transform( Vector3.Zero, Rotation.Identity ); // Clothing go offset too
+
+					var collider = Components.Get<BoxCollider>( FindMode.EverythingInSelfAndChildren );
+					collider.Enabled = true;
+
+					_isTransitioning = false;
+				}
+			}
+		}
+	}
+
+
+	private TimeSince lastStepped;
+	private void OnFootstep( SceneModel.FootstepEvent e )
+	{
+		if ( lastStepped < 0.2f )
+			return;
+
+		var pos = Transform.Position;
+		var tr = Scene.Trace.Ray( pos + Vector3.Up * 10, pos + Vector3.Down * 10 )
+			.Radius( 1 )
+			.WithoutTags( "trigger" )
+			.IgnoreGameObjectHierarchy( GameObject )
+			.Run();
+
+		if ( !tr.Hit || tr.Surface == null )
+			return;
+
+		lastStepped = 0;
+
+		var path = e.FootId == 0
+			? tr.Surface.Sounds.FootLeft
+			: tr.Surface.Sounds.FootRight;
+
+		if ( string.IsNullOrEmpty( path ) )
+			return;
+
+		var sound = Sound.Play( path, tr.HitPosition + tr.Normal * 5 );
+		sound.Volume *= e.Volume;
 	}
 }
