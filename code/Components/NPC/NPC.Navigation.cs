464b61b6a839ad0b3959474d39c8c5a2b0f9c673
yuberee <59583743+yuberee@users.noreply.github.com>
2024-03-08 23:43:57 +0100

    Cardinal directions and interest

--- code/Components/NPC/NPC.Navigation.cs
+++ code/Components/NPC/NPC.Navigation.cs
@@ -3,6 +3,18 @@ using Sauna;
 
 public partial class NPC
 {
+	static Vector3[] _possibleDirections =
+	{
+		Vector3.Forward,								// North
+		( Vector3.Forward + Vector3.Left ) / 2f,		// North-East
+		Vector3.Left,									// East
+		( Vector3.Left + Vector3.Backward ) / 2f,		// South-East
+		Vector3.Backward,								// South
+		( Vector3.Backward + Vector3.Right ) / 2f,		// South-West
+		Vector3.Right,									// West
+		( Vector3.Right + Vector3.Forward ) / 2f,		// North-West
+	};
+
 	public virtual void ComputeNavigation()
 	{
 		if ( MoveHelper == null ) return;
@@ -16,10 +28,19 @@ public partial class NPC
 			var positionDifference = TargetPosition - Transform.Position;
 			var wishDirection = (movement3D ? positionDifference.WithZ( 0f ) : positionDifference).Normal;
 
+			var interest = getInterest( wishDirection );
+
 			MoveHelper.WishVelocity = wishDirection * WalkSpeed;
 		}
 
 		MoveHelper.Move();
+	}
+
+	float[] getInterest( Vector3 direction )
+	{
+		return _possibleDirections
+			.Select( x => direction.Dot( x ) )
+			.ToArray();
 	}
 
 	void CheckNewTargetPos()
