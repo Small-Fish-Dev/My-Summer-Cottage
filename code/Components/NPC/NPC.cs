e7400db328483587bf91335b90a4109472de53b0
yuberee <59583743+yuberee@users.noreply.github.com>
2024-03-09 14:53:46 +0100

    Tweak NPC animation scale

--- code/Components/NPC/NPC.cs
+++ code/Components/NPC/NPC.cs
@@ -161,10 +161,11 @@ public partial class NPC : Component
 
 		var oldX = Model.GetFloat( "move_x" );
 		var oldY = Model.GetFloat( "move_y" );
-		var scaledSpeed = MaxRunAnimationSpeed * ((GameObject.Transform.Scale.x + GameObject.Transform.Scale.y + GameObject.Transform.Scale.z) / 3f);
-		Log.Info( scaledSpeed );
-		var newX = Vector3.Dot( MoveHelper.Velocity, Model.Transform.Rotation.Forward ) / scaledSpeed;
-		var newY = Vector3.Dot( MoveHelper.Velocity, Model.Transform.Rotation.Right ) / scaledSpeed;
+		var scaledXSpeed = MaxRunAnimationSpeed * GameObject.Transform.Scale.x;
+		var scaledYSpeed = MaxRunAnimationSpeed * GameObject.Transform.Scale.y;
+
+		var newX = Vector3.Dot( MoveHelper.Velocity, Model.Transform.Rotation.Forward ) / scaledXSpeed;
+		var newY = Vector3.Dot( MoveHelper.Velocity, Model.Transform.Rotation.Right ) / scaledYSpeed;
 		var x = MathX.Lerp( oldX, newX, Time.Delta * 5f );
 		var y = MathX.Lerp( oldY, newY, Time.Delta * 5f );
 
