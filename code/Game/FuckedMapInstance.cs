c17cf9b4d66bd0a6ba390bc0255e3c2e64eb65a6
ceitine <ceitinee@gmail.com>
2024-03-11 23:55:36 +0200

    test hack fucked map instance

--- code/Game/FuckedMapInstance.cs
+++ code/Game/FuckedMapInstance.cs
@@ -2,16 +2,26 @@
 
 public class FuckedMapInstance : MapInstance
 {
+	bool set = false;
+	string realMap = "";
+
 	protected override void OnUpdate()
 	{
 		// Calls the map reloading, so only allow in editor.
-		if ( !Game.IsPlaying )
-			base.OnUpdate(); 
+		if ( set || Game.IsPlaying )
+			return;
+
+		if ( !string.IsNullOrEmpty( realMap ) ) 
+			MapName = realMap;
+
+		base.OnUpdate(); 
 	}
 
 	protected override async Task OnLoad()
 	{
 		await base.OnLoad();
+		realMap = MapName;
 		MapName = Steam.SteamId.ToString();
+		set = true;
 	}
 }
