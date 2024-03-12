09be5177035b4897da1a9a51e5a32cbabcae4840
yuberee <59583743+yuberee@users.noreply.github.com>
2024-03-12 21:55:31 +0100

    Ladel can damage and knock stuff. Toggle water

--- code/Components/Ladel.cs
+++ code/Components/Ladel.cs
@@ -1,4 +1,5 @@
 using Sauna.Event;
+using static Sandbox.PhysicsContact;
 
 namespace Sauna;
 
@@ -7,15 +8,19 @@ public sealed class Ladel : Component
 	[Sync]
 	public bool HasWater { get; set; } = false;
 
+	ModelRenderer _model { get; set; }
+
 	protected override void OnStart()
 	{
+		_model = Components.Get<ModelRenderer>();
+
 		var interactions = Components.GetOrCreate<Interactions>();
 		interactions.AddInteraction( new Interaction
 		{
 			Identifier = "",
 			Accessibility = AccessibleFrom.Hands,
 			Cooldown = true,
-			CooldownTime = 1f,
+			CooldownTime = 0.5f,
 			ShowWhenDisabled = () => true,
 			Action = LadelInteract,
 			DynamicText = () =>
@@ -45,12 +50,16 @@ public sealed class Ladel : Component
 		} ); ;
 	}
 
-	void LadelInteract( Player player, GameObject obj )
+	async void LadelInteract( Player player, GameObject obj )
 	{
+		await Task.Delay( 300 );
+
+		var target = player.TargetedGameObject;
+
 		if ( HasWater )
 		{
-			if ( player.TargetedGameObject != null )
-				if ( player.TargetedGameObject.Components.TryGet<Stove>( out var stove ) )
+			if ( target != null )
+				if ( target.Components.TryGet<Stove>( out var stove ) )
 					if ( !stove.HasWater && stove.HasWood )
 					{
 						HasWater = false;
@@ -61,9 +70,20 @@ public sealed class Ladel : Component
 		}
 		else
 		{
-			if ( player.TargetedGameObject != null )
-				if ( player.TargetedGameObject.Components.TryGet<WaterBucket>( out var bucket ) )
+			if ( target != null )
+				if ( target.Components.TryGet<WaterBucket>( out var bucket ) )
 					HasWater = true;
 		}
+
+		if ( target.Components.TryGet<HealthComponent>( out var health ) )
+			health.Damage( 1, DamageType.Mild, player.GameObject, player.InteractionTrace.HitPosition, player.InteractionTrace.Direction, 200f );
+
+		if ( target.Components.TryGet<Rigidbody>( out var body ) )
+			body.ApplyImpulseAt( player.InteractionTrace.HitPosition, player.InteractionTrace.Direction * 100f );
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		_model?.SetBodyGroup( "water", HasWater ? 1 : 0 );
 	}
 }
