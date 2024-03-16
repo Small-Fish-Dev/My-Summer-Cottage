2233b42162106cc62008dbeb45d12046840d8a7e
yuberee <59583743+yuberee@users.noreply.github.com>
2024-03-16 14:50:41 +0100

    EventEnabledTrigger

---
+++ code/Event/Triggers/EventEnabledTrigger.cs
@@ -0,0 +1,12 @@
+using Sandbox;
+using Sauna;
+using Sauna.Event;
+
+/// <summary>
+/// Automaticall triggers when the event is enabled
+/// </summary>
+[Icon( "touch_app" )]
+[Category( "Events" )]
+public sealed class EventEnabledTrigger : EventTrigger
+{
+}
