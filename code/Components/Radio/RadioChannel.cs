69c2e189cc0e67c76ce4ffd727df1a573f75302f
ceitine <ceitinee@gmail.com>
2024-03-16 04:35:24 +0200

    bwing bwack wadio channews

--- code/Components/Radio/RadioChannel.cs
+++ code/Components/Radio/RadioChannel.cs
@@ -4,9 +4,8 @@ public struct RadioChannel
 {
 	public readonly static RadioChannel[] All = new RadioChannel[]
 	{
-		// Live radio disabled due to crashes :P
-		//new RadioChannel { Name = "YLE X", URL = "https://yleradiolive.akamaized.net/hls/live/2027674/in-YleX/256/variant.m3u8" },
-		//new RadioChannel { Name = "Järviradio FM", URL = "https://jarviradio.radiotaajuus.fi:9000/jr" },
+		new RadioChannel { Name = "YLE X", URL = "https://yleradiolive.akamaized.net/hls/live/2027674/in-YleX/256/variant.m3u8" },
+		new RadioChannel { Name = "Järviradio FM", URL = "https://jarviradio.radiotaajuus.fi:9000/jr" },
 		new RadioChannel { Name = "Zakkujo - Ero vaimosta", URL = "sounds/music/zakkujo-ero_vaimosta.sound", Local = true },
 		new RadioChannel { Name = "Gus and the Ganders - Surfin' 81", URL = "sounds/music/dawdle-gus_and_the_ganders_-_surfin_81__game.sound", Local = true },
 	};
