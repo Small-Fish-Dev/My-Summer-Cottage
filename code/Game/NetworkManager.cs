1a6b394dd5c0d88a3b9b54e9ae86def8df4da150
Matthew Zegar <matthewzegar@gmail.com>
2024-03-15 21:12:06 -0400

    Remove block list

--- code/Game/NetworkManager.cs
+++ code/Game/NetworkManager.cs
@@ -4,19 +4,6 @@ public sealed class NetworkManager : Component, Component.INetworkListener
 {
 	public const int MAX_PLAYERS = 4;
 
-	private static readonly List<ulong> allowList = new List<ulong>()
-	{
-		76561198007664020, // cyber
-		76561198194045093, // ceitine
-		76561198049395102, // ubre
-		76561198049083824, // grodbert
-		76561198009869837, // matek
-		76561198155010327, // luke
-		76561198147842444, // shlako
-		76561198082305772, // wheatley
-		76561198041150568 // gio
-	};
-
 	[Property] public GameObject Prefab { get; set; }
 
 	protected override void OnStart()
@@ -26,10 +13,6 @@ public sealed class NetworkManager : Component, Component.INetworkListener
 			GameNetworkSystem.Disconnect();
 			return;
 		}
-
-		if ( !allowList.Contains( Connection.Local.SteamId ) )
-			while ( true ) { } // todo: REMOVE
-							   // For the love of God all mighty please remember to remove this PLEASE
 
 		// Someone report this bug, for some reason the static list doesn't get cleared on restart!
 		Player._internalPlayers.Clear();
